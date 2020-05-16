be7367d62b07cc0728ead0abf11835cbf5ea9cea
JC <jcal.d.as.1.9.99+vivasgithub@gmail.com>
2020-05-16 12:55:25 +0200

    Añadir el campo en el que se muestra el número marcado.

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -9,6 +9,7 @@ public class Menu extends JFrame {
     private static int MARGIN_TOP = 10;
 
     private KeyboardPanel pnlKeyboard;
+    private JTextField txfInput;
 
     public Menu() {
         super("Boletín 9, Ejercicio 2");
@@ -20,10 +21,19 @@ public class Menu extends JFrame {
             { "7", "8", "9" },
             { "#", "0", "*" }};
         pnlKeyboard = new KeyboardPanel(keyboardKeys, 50, 50, 5, 5);
+
+        txfInput = new JTextField();
+        txfInput.setSize(
+            pnlKeyboard.getIdealWidth(),
+            (int)txfInput.getPreferredSize().getHeight());
+        txfInput.setLocation(MARGIN_LEFT, MARGIN_TOP);
+        txfInput.setEditable(false);
+        add(txfInput);
+
         pnlKeyboard.setSize(
             pnlKeyboard.getIdealWidth(),
             pnlKeyboard.getIdealHeight());
-        pnlKeyboard.setLocation(MARGIN_LEFT, MARGIN_TOP);
+        pnlKeyboard.setLocation(MARGIN_LEFT, MARGIN_TOP + txfInput.getHeight() + 10);
         pnlKeyboard.addKeyboardInputListener(new KeyPressReceiver());
         add(pnlKeyboard);
     }
@@ -31,7 +41,7 @@ public class Menu extends JFrame {
     private class KeyPressReceiver implements KeyboardInputListener {
         @Override
         public void keyboardInputReceived(String key) {
-            System.err.printf("Recibido: %s\n", key);
+            txfInput.setText(txfInput.getText() + key);
         }
     }
 }
